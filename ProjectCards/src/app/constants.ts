56df0e4b52a11a10837a12781534aa1753775f41
Levent Oz <leventoz@gmail.com>
2018-12-02 16:40:42 -0800

    Add card width constant

--- ProjectCards/src/app/constants.ts
+++ ProjectCards/src/app/constants.ts
@@ -1,7 +1,7 @@
 export default class Constants {
     static readonly useHttps = true;
     static readonly maxPageSize = 10;
-    static readonly coverSizeWidth = 300;
+    static readonly coverSizeWidth = 400; // Ensure that this value is the same as $card-width-px in _constants.scss
     static readonly coverSizeHeight = 200;
     static readonly avatarWidth = 40;    
     static readonly avatarHeight = 40;
